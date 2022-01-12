a474cd7ffee2768740bb0314748dbe2f5442bdfd
georgisg99@gmail.com <georgisg99@gmail.com>
2022-01-12 10:56:41 +0200

    Add project files.

---
+++ Project3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class GFG
+{
+    
+    static bool isPrime(int n)
+    {
+
+       
+        if (n <= 1)
+            return false;
+
+     
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+
+    static void printPrime(int n)
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            if (isPrime(i))
+                Console.Write(i + " ");
+        }
+    }
+
+   
+    public static void Main()
+    {
+        int n = 7;
+        printPrime(n);
+    }
+}
